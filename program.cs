434b42ea621c9a079b4e66af488619823dc42df5
Cari Licor <Carilicor@gmail.com>
2022-11-14 15:04:32 -0500

    banking deposit

--- program.cs
+++ program.cs
@@ -1,2 +1,12 @@
-Console.WriteLine("hello world");
-//console.log("hello world");
+Console.WriteLine("Welcome to the Bank");
+
+BankAccount account = new BankAccount(11000);
+
+//account.balance = 100;
+try{ 
+account.makeDeposit(-1);
+} catch (Exception ex) {
+    Console.WriteLine(ex.Message);
+}
+Console.WriteLine($"Your balance is: {account.getBalance()}");
+Console.WriteLine ($"Account Number: {account.accountNumber}");
